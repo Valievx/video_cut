72d5c1a5a88d9f37bf8a1bf330d7ffa2f53c57f9
Alexander Valiev <worxvaliev@gmail.com>
2023-12-04 18:05:16 +0500

    Fix VIDEO Duration

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from moviepy.editor import VideoFileClip, AudioFileClip, CompositeVideoClip, concatenate_videoclips
 from colorama import Fore
 from colorama import init
+from moviepy.editor import (VideoFileClip,
+                            AudioFileClip,
+                            CompositeVideoClip,
+                            concatenate_videoclips)
 
 import os
 import sys
 import time
-import math
 from dataclasses import dataclass
 
-from time_range import range_random, full_time_clip
+from time_range import range_random
 
 init()
 working_dir = os.path.sep.join(sys.argv[0].split(os.path.sep)[:-1])
@@ -17,32 +19,39 @@ working_dir = os.path.sep.join(sys.argv[0].split(os.path.sep)[:-1])
 @dataclass
 class VideoEditor:
     """Basic VideoEditor Class."""
-    path_file: str
-    audio_duration: float
+    path_video: str
+    path_audio: str
     path_directory: str
-    result_name: str
     path_futage: str
+    result_name: str
+    resolution_selection: str
 
     def video_cut(self) -> None:
         """Cut the video."""
-        if (os.path.split(self.path_file)[-1].endswith(".mp4")
-                or os.path.split(self.path_file)[-1].endswith(".avi")):
+        if (os.path.split(self.path_video)[-1].endswith(".mp4")
+                or os.path.split(self.path_video)[-1].endswith(".avi")):
             print(Fore.CYAN + '[+] Начинаю вырезку фрагмента видео')
 
+            # Создаем директорию, если она не существует
+            output_dir = os.path.join(working_dir, 'media')
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+
             for i, (start_time, end_time) in enumerate(range_random()):
-                clip = VideoFileClip(self.path_file)
+                clip = VideoFileClip(self.path_video)
                 clip_clip = clip.subclip(start_time, end_time)
                 print(Fore.CYAN + '[+] Записываю фрагмент видео...\n')
                 clip_clip.write_videofile(
                     f'{working_dir}\\media\\clip_{i}.mp4',
-                    codec='libx264'
+                    codec='libx264',
+                    audio=False
                 )
                 clip.reader.close()
                 clip.audio.reader.close_proc()
 
             print(Fore.GREEN + '\n[+] Видео успешно сохранены')
 
-    def video_merge(self):
+    def video_merge(self) -> None:
         """Merge the video."""
         if os.path.exists(self.path_directory):
             print(Fore.CYAN + '[+] Сканирование директории')
@@ -62,49 +71,87 @@ class VideoEditor:
                 print(
                     Fore.YELLOW + f'[+] Найдено файлов: {len(clip_to_merge)}')
 
-                print(Fore.LIGHTMAGENTA_EX + 'Начинаю генерировать новое видео')
-                clip_to_merge = (clip_to_merge * math.trunc(
-                    self.audio_duration / full_time_clip))
                 merge_final = concatenate_videoclips(
-                    clip_to_merge, method='compose'
+                    clip_to_merge,
+                    method='compose'
                 )
+
                 print(Fore.YELLOW + (
                     f'[+] Длительность объединяемого видео: '
                     f'{time.strftime("%H:%M:%S", time.gmtime(merge_final.duration))}'
                     f'\n[+] Начинаю объединение файлов...\n'))
-                merge_final.write_videofile(f'{self.result_name}.mp4')
+
+                merge_final.write_videofile(
+                    f'{self.result_name}.mp4',
+                    codec='libx264',
+                    audio=False
+                )
                 print(Fore.GREEN + '\n[+] Объединение файлов завершено')
                 print(
                     Fore.GREEN + f'[+] Видео {self.result_name}.mp4 сохранено')
 
-    def add_fx(self):
+    def add_fx(self) -> None:
         """Adding futage to video."""
-        print(Fore.LIGHTMAGENTA_EX + '[+] Накладываем футаж')
+        print(Fore.LIGHTMAGENTA_EX + '[+] Добавляем футаж')
 
-        video = VideoFileClip(f'{working_dir}\\{self.result_name}.mp4')
-        futage = VideoFileClip(self.path_futage).set_duration(video.duration).set_opacity(0.5)
-        final = CompositeVideoClip([video, futage])
-        final.write_videofile(f'{working_dir}\\{self.result_name}_fx.mp4')
+        video = VideoFileClip(f'{self.result_name}.mp4')
+        futage = VideoFileClip(
+            self.path_futage).set_duration(video.duration).set_opacity(0.5)
+        video_with_futage = CompositeVideoClip([video, futage])
+        video_with_futage.write_videofile(f'{self.result_name}_fx.mp4')
+        os.remove(f'{self.result_name}.mp4')
 
-    def add_audio(self):
+    def full_video_generation(self) -> None:
         """Adding an audio track to a video."""
-        print(Fore.LIGHTMAGENTA_EX + '[+] Заменяем Аудио')
+        print(
+            Fore.LIGHTMAGENTA_EX + '[+] Генерируем видео и добавляем аудио')
 
         video_path = f'{working_dir}\\{self.result_name}_fx.mp4'
-        audio_path = f'{working_dir}\\song.mp3'
-        final_path = f'{working_dir}\\{self.result_name}_master.mp4'
+        audio_path = self.path_audio
+        output_path = f'{working_dir}\\{self.result_name}.mp4'
 
-        video = VideoFileClip(video_path)
-        audio = AudioFileClip(audio_path)
+        video_clip = VideoFileClip(video_path)
+        audio_clip = AudioFileClip(audio_path)
 
-        duration = video.duration
-        start_time = 0
-        end_time = duration
+        video_duration = video_clip.duration
+        audio_duration = audio_clip.duration
 
-        audio_cut = audio.subclip(start_time, end_time)
-        video_without_audio = video.without_audio()
-        video_with_new_audio = video_without_audio.set_audio(audio_cut)
-        video_with_new_audio.write_videofile(final_path)
+        # Вычисление количества повторений видео,
+        # чтобы оно было не короче аудио
+        num_repeats = int(audio_duration / video_duration)
+
+        # Зацикливание видео
+        looped_video_clips = [video_clip] * num_repeats
+
+        # Вычисление остаточной длительности видео для последнего повторения
+        remaining_duration = audio_duration - (num_repeats * video_duration)
+        if remaining_duration > 0:
+            last_clip = video_clip.subclip(0, remaining_duration)
+            looped_video_clips.append(last_clip)
+
+        final_video_clip = concatenate_videoclips(looped_video_clips)
+
+        # Изменение разрешения
+        new_resolution = (1440, 1080)
+        if self.resolution_selection == 'y':
+            final_video_clip = final_video_clip.resize(new_resolution)
+            print(
+                Fore.LIGHTMAGENTA_EX + (f'\nРазрешение изменено '
+                                        f'на {new_resolution}.'))
+        else:
+            print(
+                Fore.LIGHTMAGENTA_EX + '\nРазрешение оставлено без изменений.')
+
+        final_clip = final_video_clip.set_audio(audio_clip)
+        final_clip.write_videofile(
+            output_path,
+            codec='libx264',
+            audio_codec='aac'
+        )
+
+        video_clip.reader.close()
+        video_clip.audio.reader.close_proc()
+        os.remove(f'{self.result_name}_fx.mp4')
 
 
 def main() -> None:
@@ -113,17 +160,19 @@ def main() -> None:
         input(Fore.LIGHTMAGENTA_EX + '(Пример: '
                                      'C:\\desktop\\video\\video.mp4)\n'
                                      'Введите полный путь видеофайла: '),
-        int(AudioFileClip(input('(Пример: '
-                                'C:\\desktop\\music\\song.mp3)\n '
-                                'Введите полный путь аудиофайла: ')).duration),
-        f'{working_dir}\\media',
+        input(Fore.LIGHTMAGENTA_EX + '(Пример: '
+                                     'C:\\desktop\\music\\song.mp3)\n'
+                                     'Введите полный путь аудиофайла: '),
+        os.path.join(working_dir, 'media'),
+        f'{working_dir}\\futage.mp4',
         input('Введите название нового видео: '),
-        f'{working_dir}\\futage.mp4'
+        input(
+            "Желаете изменить разрешение видео на 1440x1080? (Y/N): ").lower()
     )
     video.video_cut()
     video.video_merge()
     video.add_fx()
-    video.add_audio()
+    video.full_video_generation()
 
 
 if __name__ == "__main__":
--- time_range.py
+++ time_range.py
@@ -1,7 +1,7 @@
 from random import randint as random
 
 
-def range_random():
+def range_random() -> list:
     """Function for generating random ranges."""
     a = [random(36, 46), random(72, 82), random(96, 106)]
     b = [random(46, 56), random(82, 92), random(106, 108)]
@@ -9,10 +9,3 @@ def range_random():
     time_range = [(x, y) for x, y in zip(a, b)]
 
     return time_range
-
-
-# Вызываем функцию и получаем случайные значения
-time_ranges = range_random()
-
-# Считаем общую сумму и вычисляем разницу между вторым и первым значением в каждом кортеже
-full_time_clip = sum([y - x for x, y in time_ranges])
